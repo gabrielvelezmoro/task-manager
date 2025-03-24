101a9680a48fcc44f8fe4078a4dcaef86bdc3dbf
Gabriel Velezmoro <gabrieuvelezmoro@gmail.com>
2025-03-24 00:40:20 -0300

    fix: remove password from created user response

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { UserDTO } from './user.dto';
-import { v4 as uuid } from 'uuid';
+import { CreateUserRequestDTO, UserDTO } from './user.dto';
 import { hashSync as bcryptHashSync } from 'bcrypt';
 import { PrismaService } from '../prisma.service';
 
@@ -8,10 +7,17 @@ import { PrismaService } from '../prisma.service';
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(user: UserDTO) {
-    user.id = uuid();
+  async create(user: CreateUserRequestDTO) {
     user.passwd = bcryptHashSync(user.passwd, 10);
-    const newUser = await this.prisma.user.create({ data: user });
+    const newUser = await this.prisma.user.create({
+      data: user,
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+
     return newUser;
   }
 
